c5e00f0539f9a3148bd0012a7a9a9b321f5375a6
zzd <zdzheng12@gmail.com>
2018-12-06 11:44:35 -0800

    final

--- demo.sh
+++ demo.sh
@@ -1,1 +1,6 @@
-python fid_score.py ../Market/pytorch/train ../Market/pytorch/query --gpu 0
+#python fid_score.py ../Market/pytorch/train_all ../Market/pytorch/gallery --gpu 0
+#python fid_score.py ../Market/pytorch/train_all ../cloud/DCGAN-pytorch/result --gpu 0
+#python fid_score.py ../Market/pytorch/train_all ../cloud/Market/pytorch/gan_id --gpu 0
+python fid_score.py ../Market/pytorch/train_all ../cloud/Market/pytorch/gan_id-noloop --gpu 0
+#python fid_score.py ../Market/pytorch/train_all ../PG2-GAN-testresult/G2 --gpu 0
+#python fid_score.py ../Market/pytorch/train_all ../FD-GAN/test-class --gpu 0
